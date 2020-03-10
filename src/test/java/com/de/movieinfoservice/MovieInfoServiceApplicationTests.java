e0e34ca7d75cdda15ba4903e6ac9ef8f5d3a9c89
Bhushan Dafale <Bhushand@pune.smartek21.st21>
2020-03-10 12:55:32 +0530

    Initial commit

---
+++ src/test/java/com/de/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.de.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
